e7b677afaca78bf1ef62bc62c60e37caf02b9e0c
Richard Malca <rmalcac16_1@unc.edu.pe>
2026-09-20 12:44:52 -0500

    fix: remove maximum-scale=1 to allow user zooming and satisfy accessibility audit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 
         <meta name="theme-color" content="#ffffff">
 
